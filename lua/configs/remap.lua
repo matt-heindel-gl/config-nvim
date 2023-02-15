b224fed14bc7054c44ef9d3d9ee8547e2f762972
Matt Heindel <mheindel@goodleap.com>
2023-02-15 13:21:13 -0800

    more remaps and settings

--- lua/configs/remap.lua
+++ lua/configs/remap.lua
@@ -1,3 +1,20 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>cd', vim.cmd.Ex)
 
+-- move selected lines up/down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- append next line to current line preserving cursor position
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- paste and preserve bufer
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- yank to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- scroll half page with cursor reset
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
--- lua/configs/set.lua
+++ lua/configs/set.lua
@@ -1,14 +1,23 @@
 -- wide cursor while editing
 vim.opt.guicursor = ""
 
--- display line numbers
+-- display *relative* line numbers
 vim.opt.nu = true
+vim.opt.relativenumber = true
 
 -- display file name
 vim.opt.title = true
 
 -- use system clipboard
 vim.opt.clipboard = 'unnamed'
+
+-- next search result, cursor centered
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- scroll, cursor centered
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- tab width
 vim.opt.tabstop = 2
@@ -31,11 +40,19 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
+-- scrolling buffer
 vim.opt.scrolloff = 4
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+-- ruler
+vim.opt.colorcolumn = "100"
+
+-- search and replace current word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- case insensitive by default
+vim.api.nvim_command('set ignorecase')
 
