55827f6e163520770787d0326cd702396d2f3587
Matt Heindel <mheindel@goodleap.com>
2023-03-16 17:48:29 -0700

    remap for git blame

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,8 +1,11 @@
 -- Git
 
--- open Git in new tab
+-- status
 vim.keymap.set("n", "<leader>gs", ":tab G<CR>")
 
 -- diff
 vim.keymap.set("n", "<leader>gd", ":tab G diff<CR>")
+
+-- blame
+vim.keymap.set("n", "<leader>gb", ":G blame<CR>")
 
