fb4d0b28d75f03f39ff01e466299824cafde6881
Matt Heindel <mheindel@goodleap.com>
2023-04-07 10:50:29 -0700

    gitgutter remaps

---
+++ after/plugin/gitgutter.lua
@@ -0,0 +1,8 @@
+-- Git
+
+-- git file diff
+vim.keymap.set("n", "<leader>fd", ":GitGutterDiffOrig<CR>")
+
+-- line highlight
+vim.keymap.set("n", "<leader>lh", ":GitGutterLineHighlightsToggle<CR>")
+
