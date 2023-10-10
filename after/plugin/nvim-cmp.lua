cdd99ec3882244fa915015f53147a14ae069ea86
Matt Heindel <mheindel@goodleap.com>
2023-10-09 17:10:30 -0700

    auto complete select first by default

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -2,16 +2,6 @@ local cmp = require("cmp")
 
 cmp.setup({
   mapping = {
-     ["<CR>"] = cmp.mapping({
-       i = function(fallback)
-         if cmp.visible() and cmp.get_active_entry() then
-           cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-         else
-           fallback()
-         end
-       end,
-       s = cmp.mapping.confirm({ select = true }),
-       c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-     }),
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
   }
 })
